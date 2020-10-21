97011cc6bf4e893b0c1d644566ae4aa97105649b
pfedick <patrick@pfp.de>
2020-10-21 07:51:17 +0200

    Amount of queries per thread is calculated individually for each thread

--- src/main.cpp
+++ src/main.cpp
@@ -479,8 +479,13 @@ void DNSSender::run(int queryrate)
 	}
 
 	ppl7::ThreadPool::iterator it;
+	int queries_rest=queryrate;
+	int threads_rest=ThreadCount;
 	for (it=threadpool.begin();it!=threadpool.end();++it) {
-		((DNSSenderThread*)(*it))->setQueryRate(queryrate/ThreadCount);
+		int queries_thread=queries_rest/threads_rest;
+		threads_rest--;
+		queries_rest-=queries_thread;
+		((DNSSenderThread*)(*it))->setQueryRate(queries_thread);
 		((DNSSenderThread*)(*it))->setTimeslice(Timeslices);
 	}
 	vis_prev_results.clear();
